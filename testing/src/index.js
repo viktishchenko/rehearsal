394c06c80f2b8523356eccc2a273e481f0295fbb
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-09 17:58:53 +0600

    refactor: first component

--- testing/src/index.js
+++ testing/src/index.js
@@ -2,10 +2,6 @@
 import { createRoot } from "react-dom/client";
 import "./index.css";
 
-// const Greeting = () => {
-//   return React.createElement("div", {}, React.createElement("h1", {}, "halo"));
-// };
-
 function BookList() {
   return (
     <section className="booklist">
@@ -17,21 +13,15 @@ function BookList() {
 const Book = () => {
   return (
     <article className="book">
-      <Image />
-      <Title />
-      <Author />
+      <img
+        className="img"
+        src="https://images-na.ssl-images-amazon.com/images/I/914QgVSAVTL._AC_UL200_SR200,200_.jpg"
+        alt="book"
+      />
+      <h3>Little Blue Truck's Halloween: A Halloween </h3>
+      <h4>Alice Schertle</h4>
     </article>
   );
 };
-
-const Image = () => (
-  <img
-    className="img"
-    src="https://images-na.ssl-images-amazon.com/images/I/914QgVSAVTL._AC_UL200_SR200,200_.jpg"
-    alt="book"
-  />
-);
-const Title = () => <h3>Little Blue Truck's Halloween: A Halloween </h3>;
-const Author = () => <h4>Alice Schertle</h4>;
 
 createRoot(document.getElementById("root")).render(<BookList />);
