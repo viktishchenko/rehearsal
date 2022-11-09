a3ac3fd0ef6643672f94ef1bf2dc0f3d9143a8c5
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-09 18:58:12 +0600

    feat: pass props ex

--- testing/src/index.js
+++ testing/src/index.js
@@ -1,29 +1,42 @@
 // import React from "react";
 import { createRoot } from "react-dom/client";
+
+// CSS
 import "./index.css";
 
-function BookList() {
+// setup vars
+const author = "Alice Schertle";
+const title = "Little Blue Truck's Halloween: A Halloween";
+const img =
+  "https://images-na.ssl-images-amazon.com/images/I/914QgVSAVTL._AC_UL200_SR200,200_.jpg";
+
+function BookList(shakeAndBake) {
+  console.log("shakeAndBake", shakeAndBake); // {}
   return (
     <section className="booklist">
-      <Book />
+      {/*
+      props >> Object { job: "developer" }
+       */}
+      <Book job="developer" />
+      {/* 
+      props >> Object { title: "random", number: 22 }
+       */}
+      <Book title="random" number={20 + 2} />
     </section>
   );
 }
 
-const author = "Alice Schertle";
-
-const Book = () => {
-  const title = "Little Blue Truck's Halloween: A Halloween";
+const Book = (props) => {
+  console.log("props", props); // {}
   return (
     <article className="book">
-      <img
-        className="img"
-        src="https://images-na.ssl-images-amazon.com/images/I/914QgVSAVTL._AC_UL200_SR200,200_.jpg"
-        alt="book"
-      />
+      <img className="img" src={img} alt="book" />
       <h3>{title}</h3>
       <h4>{author.toUpperCase()}</h4>
       <p>{5 + 5}</p>
+      <p>{props.job}</p>
+      <p>{props.title}</p>
+      <p>{props.number}</p>
     </article>
   );
 };
