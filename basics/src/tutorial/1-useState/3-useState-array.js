4810dd9f0c7c8a642d9982e31483e1ae073937e1
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-11 06:50:04 +0600

    feat: 3 tutor add remove from list func-ty

--- basics/src/tutorial/1-useState/3-useState-array.js
+++ basics/src/tutorial/1-useState/3-useState-array.js
@@ -1,18 +1,44 @@
-import React, { useState } from "react";
-import { data as second } from "../../data";
+import React from "react";
+// import { useState } from "react";
+import { data } from "../../data";
+
+/* anfn, nfn Ctrl + Space */
 
 const UseStateArray = () => {
-  const [people, setPeople] = useState(second);
+  const [people, setPeople] = React.useState(data);
+
+  const removeItem = (id) => {
+    let newPeople = people.filter((person) => {
+      return person.id !== id;
+    });
+    setPeople(newPeople);
+  };
   return (
     <>
       {people.map((person) => {
         const { id, name } = person;
         return (
           <div className="item" key={id}>
-            <h3>{name}</h3>
+            <h3 style={{ marginBottom: "0" }}>{name}</h3>
+            <button
+              style={{ color: "red" }}
+              onClick={() => {
+                removeItem(id);
+              }}
+            >
+              x
+            </button>
           </div>
         );
       })}
+      <button
+        className="btn"
+        onClick={() => {
+          setPeople([]);
+        }}
+      >
+        clear all item
+      </button>
     </>
   );
 };
