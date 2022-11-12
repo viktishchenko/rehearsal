13cb0f8cf0b6e757c2b3459cbe1048fce6ea7f3c
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-12 07:05:58 +0600

    feat: react access input data

--- basics/src/tutorial/4-forms/1-controlled-inputs.js
+++ basics/src/tutorial/4-forms/1-controlled-inputs.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 /* 
     JS
@@ -9,9 +9,12 @@ value, onChange
 */
 
 const ControlledInputs = () => {
+  const [firstName, setFirstName] = useState("");
+  const [email, setEmail] = useState("");
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log("hello world");
+    console.log(firstName, email);
   };
   return (
     <>
@@ -19,11 +22,27 @@ const ControlledInputs = () => {
         <form action="" className="form" onSubmit={handleSubmit}>
           <div className="form-control">
             <label htmlFor="firstName">Name: </label>
-            <input type="text" name="firstName" id="firstName" />
+            <input
+              type="text"
+              name="firstName"
+              id="firstName"
+              value={firstName}
+              onChange={(e) => {
+                setFirstName(e.target.value);
+              }}
+            />
           </div>
           <div className="form-control">
             <label htmlFor="email">Email: </label>
-            <input type="email" name="email" id="email" />
+            <input
+              type="email"
+              name="email"
+              id="email"
+              value={email}
+              onChange={(e) => {
+                setEmail(e.target.value);
+              }}
+            />
           </div>
           <button type="submit">add person</button>
         </form>
