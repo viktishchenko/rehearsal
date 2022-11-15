a48d101d4bcee6b02d59cb8048a53f9899a335b2
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-15 18:21:13 +0600

    feat: 3 more variants to check data

--- basics/src/tutorial/10-prop-types/Product.js
+++ basics/src/tutorial/10-prop-types/Product.js
@@ -1,27 +1,47 @@
 // imr, ipt
 import React from "react";
-import propTypes from "prop-types";
+// import propTypes from "prop-types";
+import defaultImage from "../../assets/default-image.jpeg";
 
-const Product = ({ image, name, price }) => {
+const Product = ({
+  // image,
+  // name,
+  // price,
+  image = { defaultImage },
+  name = "default name",
+  price = 3.55,
+}) => {
   /* 
     CHECK DATA WITH propsTypes: image, name, price
   */
 
-  console.log("image,name,price>>", image, name, price);
+  // console.log("image>>", image);
+  // const url = image && image.url;
+  // console.log("url>>", url);
+
   return (
     <article className="product">
-      <h4>single product</h4>
-      {/* <img src={image.url} alt={name} />
+      <img src={image.url || defaultImage} alt={name} />
+      {/* <img src={image.url || defaultImage} alt={name || "default name"} /> */}
+      {/* <img src={url || defaultImage} alt={name || "default name"} /> */}
+      {/* {image.url ? <img src={image.url} alt={name} /> : "not found"} */}
       <h4>{name}</h4>
-      <p>$ {price}</p> */}
+      <p>$ {price}</p>
     </article>
   );
 };
 
-Product.propTypes = {
-  image: propTypes.object.isRequired,
-  name: propTypes.string.isRequired,
-  price: propTypes.number.isRequired,
-};
+// Product.propTypes = {
+//   image: propTypes.object.isRequired,
+//   name: propTypes.string.isRequired,
+//   price: propTypes.number.isRequired,
+// };
+
+// add default props
+// Product.defaultProps = {
+//   name: "default name",
+//   price: 3.99,
+//   image: { defaultImage },
+// };
 
 export default Product;
