0ac1b509ecec903a8828f693ef514467f81e2759
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-15 14:55:02 +0600

    feat: check data w prop-types

--- basics/src/tutorial/10-prop-types/Product.js
+++ basics/src/tutorial/10-prop-types/Product.js
@@ -1,6 +1,10 @@
+// imr, ipt
+import React from "react";
+import propTypes from "prop-types";
+
 const Product = ({ image, name, price }) => {
   /* 
-    CHECK DATA: image, name, price
+    CHECK DATA WITH propsTypes: image, name, price
   */
 
   console.log("image,name,price>>", image, name, price);
@@ -12,6 +16,12 @@ const Product = ({ image, name, price }) => {
       <p>$ {price}</p> */}
     </article>
   );
+};
+
+Product.propTypes = {
+  image: propTypes.object.isRequired,
+  name: propTypes.string.isRequired,
+  price: propTypes.number.isRequired,
 };
 
 export default Product;
