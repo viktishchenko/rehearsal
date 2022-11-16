106b324184a75eec706ef484a48c842aa2923c2e
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-16 07:57:54 +0600

    feat: display nested component

--- basics/src/tutorial/11-react-router/pages/Dashboard.js
+++ basics/src/tutorial/11-react-router/pages/Dashboard.js
@@ -1,7 +1,10 @@
+import { Outlet } from "react-router-dom";
+
 const Dashboard = () => {
   return (
     <section className="section">
       <h2>Dashboard</h2>
+      <Outlet />
     </section>
   );
 };
--- basics/src/tutorial/11-react-router/pages/Products.js
+++ basics/src/tutorial/11-react-router/pages/Products.js
@@ -1,7 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Products = () => {
   return (
     <section className="section">
       <h2>products</h2>
+      <Link to="/" className="btn">
+        home →
+      </Link>
     </section>
   );
 };
