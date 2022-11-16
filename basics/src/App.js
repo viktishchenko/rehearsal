fb3ff146a16462cae8fd757254f00397bed3bfb8
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-16 07:43:58 +0600

    feat: nested routes

--- basics/src/App.js
+++ basics/src/App.js
@@ -2,7 +2,9 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./tutorial/11-react-router/pages/Home";
 import About from "./tutorial/11-react-router/pages/About";
+import Dashboard from "./tutorial/11-react-router/pages/Dashboard";
 import Products from "./tutorial/11-react-router/pages/Products";
+import SingleProduct from "./tutorial/11-react-router/pages/SingleProduct";
 import Error from "./tutorial/11-react-router/pages/Error";
 
 function App() {
@@ -10,8 +12,12 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/products" element={<Products />} />
+        <Route path="about" element={<About />} />
+        <Route path="products" element={<Products />} />
+        <Route path="dashboard" element={<Dashboard />}>
+          <Route path="singleproduct" element={<SingleProduct />} />
+          <Route path="stats" element={<div>stats...</div>} />
+        </Route>
         <Route path="*" element={<Error />} />
       </Routes>
     </BrowserRouter>
--- basics/src/tutorial/11-react-router/pages/Dashboard.js
+++ basics/src/tutorial/11-react-router/pages/Dashboard.js
@@ -1,7 +1,7 @@
 const Dashboard = () => {
   return (
     <section className="section">
-      <h2>Dashboerd</h2>
+      <h2>Dashboard</h2>
     </section>
   );
 };
