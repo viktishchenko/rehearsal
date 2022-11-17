fae66bf32662bf91b62ea47227c0f1de653a5ed8
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-17 08:04:03 +0600

    feat: display single product

--- basics/src/tutorial/11-react-router/pages/Products.js
+++ basics/src/tutorial/11-react-router/pages/Products.js
@@ -1,12 +1,21 @@
 import { Link } from "react-router-dom";
+import products from "../../../data";
 
 const Products = () => {
   return (
     <section className="section">
       <h2>products</h2>
-      <Link to="/" className="btn">
-        home →
-      </Link>
+      <div className="products">
+        {products.map((product) => {
+          const { id, name } = product;
+          return (
+            <article key={id}>
+              <h5>{name}</h5>
+              <Link to={`/products/${id}`}>more info →</Link>
+            </article>
+          );
+        })}
+      </div>
     </section>
   );
 };
--- basics/src/tutorial/11-react-router/pages/SingleProduct.js
+++ basics/src/tutorial/11-react-router/pages/SingleProduct.js
@@ -1,10 +1,18 @@
 import { Link, useParams } from "react-router-dom";
+import products from "../../../data";
 
 const SingleProduct = () => {
   const { productId } = useParams();
+  const product = products.find((product) => {
+    return product.id === productId;
+  });
+
+  const { image, name } = product;
+
   return (
     <section className="section product">
-      <h2>single product {productId}</h2>
+      <img src={image} alt={name} />
+      <h5>{name}</h5>
       <Link to="/products">back to products</Link>
     </section>
   );
