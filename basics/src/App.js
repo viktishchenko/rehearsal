a32b973f7cc6f8b8603618142b629fd3b50b79f7
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-18 15:51:01 +0600

    feat: add nested route approach

--- basics/src/App.js
+++ basics/src/App.js
@@ -10,6 +10,7 @@ import Login from "./tutorial/11-react-router/pages/Login";
 import SharedLayout from "./tutorial/11-react-router/pages/SharedLayout";
 import { useState } from "react";
 import ProtectedRoute from "./tutorial/11-react-router/pages/ProtectedRoute";
+import SharedProductLayout from "./tutorial/11-react-router/pages/SharedProductLayout";
 
 function App() {
   const [user, setUser] = useState(null);
@@ -20,8 +21,10 @@ function App() {
         <Route path="/" element={<SharedLayout />}>
           <Route index element={<Home />} />
           <Route path="/about" element={<About />} />
-          <Route path="/products" element={<Products />} />
-          <Route path="/products/:productId" element={<SingleProduct />} />
+          <Route path="/products" element={<SharedProductLayout />}>
+            <Route index element={<Products />} />
+            <Route path=":productId" element={<SingleProduct />} />
+          </Route>
           <Route path="/login" element={<Login setUser={setUser} />} />
           <Route
             path="/dashboard"
