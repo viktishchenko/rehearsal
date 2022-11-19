895f63d1814d77081531f8b9699ee5f7e7cfa2bc
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-19 13:59:17 +0600

    feat: axios w headers

--- basics/src/tutorial/13-axios/examples/2-headers.js
+++ basics/src/tutorial/13-axios/examples/2-headers.js
@@ -1,13 +1,25 @@
 import { useState } from "react";
+import axios from "axios";
 
-const url = "https://icanhazdadjoke.com/";
+const url = "https://api.chucknorris.io/jokes/random";
+
 // Accept : 'application/json'
 
 const Headers = () => {
   const [joke, setJoke] = useState("random dad joke");
 
   const fetchJoke = async () => {
-    console.log("fetch dad joke");
+    try {
+      const { data } = await axios(url, {
+        headers: {
+          Accept:
+            "text/html, application/xhtml+xml, application/xml;q=0.9, */*;q=0.8",
+        },
+      });
+      setJoke(data.value);
+    } catch (error) {
+      console.log(error.response);
+    }
   };
 
   return (
