ae18e964fa4cda0e9915426fad6cfb59000374d1
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-19 12:06:38 +0600

    feat: axios basic request

--- basics/src/tutorial/13-axios/examples/1-first-request.js
+++ basics/src/tutorial/13-axios/examples/1-first-request.js
@@ -1,11 +1,22 @@
 import React, { useEffect } from "react";
+import axios from "axios";
 
 // limit, if 429 wait for 15 min and try again
 const url = "https://course-api.com/react-store-products";
 
 const FirstRequest = () => {
+  const fetchData = async () => {
+    try {
+      const response = await axios(url);
+      const data = response.data;
+      console.log(data); // Array(23) [ {…}, {…}, … ]
+    } catch (error) {
+      console.log(error.response);
+    }
+  };
+
   useEffect(() => {
-    console.log("first request");
+    fetchData();
   }, []);
 
   return <h2 className="text-center">First Request</h2>;
