2364ca331e6bdfa5f7ce3de3339410c87df0303d
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-19 07:56:05 +0600

    feat: useCallback w fetch data

--- basics/src/tutorial/9-custom-hooks/2-useFetch.js
+++ basics/src/tutorial/9-custom-hooks/2-useFetch.js
@@ -1,19 +1,20 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useCallback } from "react";
 
 export const useFetch = (url) => {
   const [isLoading, setIsLoading] = useState(true);
   const [products, setProducts] = useState([]);
 
-  const getProducts = async () => {
+  const getProducts = useCallback(async () => {
     const response = await fetch(url);
     const products = await response.json();
     setProducts(products);
     setIsLoading(false);
-  };
+  }, [url]);
 
+  // w/o useCallback we get infinite loop w getProducts
   useEffect(() => {
     getProducts();
-  }, [url]);
+  }, [url, getProducts]);
 
   return { isLoading, products };
 };
