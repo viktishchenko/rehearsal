eec7a64255e59b72d41b5dec8d609ea38ba33cc3
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-21 08:26:30 +0600

    init: projects

---
+++ projects/src/App.js
@@ -0,0 +1,14 @@
+import List from "./draft/01-birth/List";
+import { data } from "./draft/01-birth/data";
+
+function App() {
+  console.log(data);
+  return (
+    <>
+      <h2>01 birth pr</h2>
+      <List />
+    </>
+  );
+}
+
+export default App;
