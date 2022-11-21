75d98c08dc1e6e1a09d4b322d4cfd5f86b83713f
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-21 18:35:02 +0600

    init: reviews pr

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,60 +1,11 @@
-import { useState, useEffect } from "react";
-import Loading from "./draft/02-tours/Loading";
-import Tours from "./draft/02-tours/Tours";
-
-const url = "https://course-api.com/react-tours-project";
+import Review from "./draft/03-reviews/Review";
 
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [tours, setTours] = useState([]);
-
-  const removeTour = (id) => {
-    const newTours = tours.filter((tour) => tour.id !== id);
-    setTours(newTours);
-  };
-
-  const fetchTours = async () => {
-    setLoading(true);
-    try {
-      const res = await fetch(url);
-      const tours = await res.json();
-      setLoading(false);
-      setTours(tours);
-    } catch (error) {
-      setLoading(false);
-      console.log(error);
-    }
-  };
-
-  useEffect(() => {
-    fetchTours();
-  }, []);
-
-  if (loading) {
-    return (
-      <main>
-        <Loading />
-      </main>
-    );
-  }
-
-  if (tours.length === 0) {
-    return (
-      <main>
-        <div className="title">
-          <h2>no tours left</h2>
-          <button className="btn" onClick={fetchTours}>
-            refresh
-          </button>
-        </div>
-      </main>
-    );
-  }
-
   return (
-    <main>
-      <Tours tours={tours} removeTour={removeTour} />
-    </main>
+    <>
+      <h2>reviews</h2>
+      <Review />
+    </>
   );
 }
 
