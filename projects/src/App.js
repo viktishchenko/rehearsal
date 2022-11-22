305c2fa8f59074e9d7ec3df91306502023161887
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-22 20:41:59 +0600

    feat: basic layout

--- projects/src/App.js
+++ projects/src/App.js
@@ -2,9 +2,40 @@ import { useState, useEffect } from "react";
 import { FiChevronRight, FiChevronLeft } from "react-icons/fi";
 import { FaQuoteRight } from "react-icons/fa";
 import data from "./draft/07-slider/data";
+
 function App() {
-  console.log("data>>", data);
-  return <h2>slider project</h2>;
+  const [people, setPeople] = useState(data);
+  const [index, setIndex] = useState(0);
+
+  return (
+    <section className="section">
+      <div className="title">
+        <h2>
+          <span>/</span>reviews
+        </h2>
+      </div>
+      <div className="section-center">
+        {people.map((person, idx) => {
+          const { id, image, name, title, quote } = person;
+          return (
+            <article key={id}>
+              <img src={image} alt={name} className="person-img" />
+              <h4>{name}</h4>
+              <p className="title">{title}</p>
+              <p className="text">{quote}</p>
+              <FaQuoteRight className="icon" />
+            </article>
+          );
+        })}
+        <button className="prev">
+          <FiChevronLeft />
+        </button>
+        <button className="next">
+          <FiChevronRight />
+        </button>
+      </div>
+    </section>
+  );
 }
 
 export default App;
