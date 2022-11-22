3a75b72c1c38ab74b0ab48074e0ee0e0a63c1c34
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-22 13:05:27 +0600

    feat: data filtering, pass props

--- projects/src/draft/05-menu/Categories.js
+++ projects/src/draft/05-menu/Categories.js
@@ -1,7 +1,18 @@
 import React from "react";
 
-const Categories = () => {
-  return <div>Categories</div>;
+const Categories = ({ filterItems }) => {
+  return (
+    <div className="btn-container">
+      <button
+        className="filter-btn"
+        onClick={() => {
+          filterItems("breakfast");
+        }}
+      >
+        breakfast
+      </button>
+    </div>
+  );
 };
 
 export default Categories;
--- projects/src/draft/05-menu/Menu.js
+++ projects/src/draft/05-menu/Menu.js
@@ -1,7 +1,6 @@
 import React from "react";
 
 const Menu = ({ items }) => {
-  console.log(items);
   return (
     <div className="section-center">
       {items.map((menuItem) => {
