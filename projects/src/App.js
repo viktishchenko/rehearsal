3b87ef75a09ee80a1f40d0f89de273b850520eda
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-22 19:37:47 +0600

    feat: loading, fetch data

--- projects/src/App.js
+++ projects/src/App.js
@@ -4,6 +4,29 @@ import { FaAngleDoubleRight } from "react-icons/fa";
 const url = "https://course-api.com/react-tabs-project";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [job, setJob] = useState([]);
+  const [value, setValue] = useState(0);
+
+  const fetchJobs = async () => {
+    const res = await fetch(url);
+    const newJobs = await res.json();
+    setJob(newJobs);
+    setLoading(false);
+  };
+
+  useEffect(() => {
+    fetchJobs();
+  }, []);
+
+  if (loading) {
+    return (
+      <section className="section loading">
+        <h1>loading...</h1>
+      </section>
+    );
+  }
+
   return <h2>tabs project</h2>;
 }
 
