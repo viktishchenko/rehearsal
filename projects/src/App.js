3b4957984af26faddeb86469a0b4dde1455bd584
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-22 13:54:04 +0600

    init: tabs project

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,36 +1,10 @@
-import { useState } from "react";
-import Menu from "./draft/05-menu/Menu";
-import Categories from "./draft/05-menu/Categories";
-import items from "./draft/05-menu/data";
+import { useState, useEffect } from "react";
+import { FaAngleDoubleRight } from "react-icons/fa";
 
-// get unique category
-const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+const url = "https://course-api.com/react-tabs-project";
 
 function App() {
-  const [menuItems, setMenuItems] = useState(items);
-  const [categories, setCategories] = useState(allCategories);
-
-  const filterItems = (category) => {
-    if (category === "all") {
-      setMenuItems(items);
-      return;
-    }
-    const newItems = items.filter((item) => item.category === category);
-    setMenuItems(newItems);
-  };
-
-  return (
-    <main>
-      <section className="menu section">
-        <div className="title">
-          <h2>menu project</h2>
-          <div className="underline"></div>
-        </div>
-        <Categories categories={categories} filterItems={filterItems} />
-        <Menu items={menuItems} />
-      </section>
-    </main>
-  );
+  return <h2>tabs project</h2>;
 }
 
 export default App;
---
+++ projects/src/draft/05-menu/App.js
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import Menu from "./draft/05-menu/Menu";
+import Categories from "./draft/05-menu/Categories";
+import items from "./draft/05-menu/data";
+
+// get unique category
+const allCategories = ["all", ...new Set(items.map((item) => item.category))];
+
+function App() {
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState(allCategories);
+
+  const filterItems = (category) => {
+    if (category === "all") {
+      setMenuItems(items);
+      return;
+    }
+    const newItems = items.filter((item) => item.category === category);
+    setMenuItems(newItems);
+  };
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>menu project</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories categories={categories} filterItems={filterItems} />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
+}
+
+export default App;
