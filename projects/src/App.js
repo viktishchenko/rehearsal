2a25b8293929370fabed2fb65d49d4547747071f
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-22 12:37:10 +0600

    feat: pass props, iterating, destructuring

--- projects/src/App.js
+++ projects/src/App.js
@@ -4,8 +4,21 @@ import Categories from "./draft/05-menu/Categories";
 import items from "./draft/05-menu/data";
 
 function App() {
-  console.log(items);
-  return <h2>menu project setup</h2>;
+  const [menuItems, setMenuItems] = useState(items);
+  const [categories, setCategories] = useState([]);
+
+  return (
+    <main>
+      <section className="menu section">
+        <div className="title">
+          <h2>menu project</h2>
+          <div className="underline"></div>
+        </div>
+        <Categories />
+        <Menu items={menuItems} />
+      </section>
+    </main>
+  );
 }
 
 export default App;
