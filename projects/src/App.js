5046d608fcdba871a910fc74deccc5c8c9787f4e
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-23 14:10:46 +0600

    feat: form data handle, slice data w amount

--- projects/src/App.js
+++ projects/src/App.js
@@ -4,8 +4,47 @@ import { useState } from "react";
 import data from "./draft/08-lorem/data";
 
 function App() {
-  console.log("data>>", data);
-  return <h2>loren project</h2>;
+  const [count, setCount] = useState(0);
+  const [text, setText] = useState([]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // important point: INPUT RETURN STRING!!!
+    let amount = parseInt(count);
+    if (count <= 0) {
+      amount = 1;
+    }
+    if (count > 8) {
+      amount = 8;
+    }
+    /* slice: end value not included */
+    setText(data.slice(0, amount));
+  };
+
+  return (
+    <section className="section-center">
+      <h3>lorem project</h3>
+      <form className="lorem-form" onSubmit={handleSubmit}>
+        <label htmlFor="amount">paragraphs:</label>
+        <input
+          type="number"
+          id="amount"
+          value={count}
+          onChange={(e) => {
+            setCount(e.target.value);
+          }}
+        />
+        <button type="submit" className="btn">
+          generate
+        </button>
+      </form>
+      <article className="lorem-text">
+        {text.map((item, idx) => {
+          return <p key={idx}>{item}</p>;
+        })}
+      </article>
+    </section>
+  );
 }
 
 export default App;
