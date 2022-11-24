5a60c2e1dcf1c3196b167c26fb0a371fdd44a07a
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-24 11:36:13 +0600

    feat: clear list func

--- projects/src/App.js
+++ projects/src/App.js
@@ -23,7 +23,7 @@ function App() {
     } else if (name && isEditing) {
       // deal w edit
     } else {
-      // show alert
+      showAlert(true, "success", "item added to list");
       const newItem = {
         id: new Date().getTime().toString(),
         title: name,
@@ -35,6 +35,11 @@ function App() {
 
   const showAlert = (show = false, type = "", msg = "") => {
     setAlert({ show, type, msg });
+  };
+
+  const clearList = () => {
+    showAlert(true, "danger", "empty list");
+    setList([]);
   };
 
   return (
@@ -60,7 +65,9 @@ function App() {
       {list.length > 0 && (
         <div className="grocery-container">
           <List items={list} />
-          <button className="clear-btn">clear items</button>
+          <button className="clear-btn" onClick={clearList}>
+            clear items
+          </button>
         </div>
       )}
     </section>
