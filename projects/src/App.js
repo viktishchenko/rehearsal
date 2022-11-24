ad1aa09443debce22ca59baa9dc8700920ff5b7c
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-24 14:49:52 +0600

    feat: navbar w dynmic data

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,56 +1,10 @@
 /* imrs */
 import { useState } from "react";
 import Navbar from "./draft/11-navbar/Navbar";
-import logo from "./draft/11-navbar/logo.svg";
-import { FaBars, FaTwitter } from "react-icons/fa";
 
 /* rfce */
-
 function App() {
-  return (
-    <nav className="nav-center">
-      <div className="nav-header">
-        <img src={logo} alt="logo" />
-        <button className="nav-toggle">
-          <FaBars />
-        </button>
-      </div>
-      <div className="links-container show-container">
-        <ul className="links">
-          <li>
-            <a href="#">home</a>
-          </li>
-          <li>
-            <a href="#">about</a>
-          </li>
-          <li>
-            <a href="#">contact</a>
-          </li>
-          <li>
-            <a href="#">products</a>
-          </li>
-        </ul>
-      </div>
-      <ul className="social-icons">
-        <li>
-          <a href="https://www.twitter.com">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com">
-            <FaTwitter />
-          </a>
-        </li>
-        <li>
-          <a href="https://www.twitter.com">
-            <FaTwitter />
-          </a>
-        </li>
-      </ul>
-      {/* <Navbar /> */}
-    </nav>
-  );
+  return <Navbar />;
 }
 
 export default App;
--- projects/src/draft/11-navbar/Navbar.js
+++ projects/src/draft/11-navbar/Navbar.js
@@ -4,7 +4,39 @@ import { links, social } from "./data";
 import logo from "./logo.svg";
 
 const Navbar = () => {
-  return <h4>Navbar</h4>;
+  return (
+    <nav className="nav-center">
+      <div className="nav-header">
+        <img src={logo} alt="logo" />
+        <button className="nav-toggle">
+          <FaBars />
+        </button>
+      </div>
+      <div className="links-container show-container">
+        <ul className="links">
+          {links.map((link) => {
+            const { id, url, text } = link;
+            return (
+              <li key={id}>
+                <a href={url}>{text}</a>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      <ul className="social-icons">
+        {social.map((item) => {
+          const { id, url, icon } = item;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+      {/* <Navbar /> */}
+    </nav>
+  );
 };
 
 export default Navbar;
