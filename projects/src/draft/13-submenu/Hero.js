02b22c2a5e42f928b3f98e1c11995efedff1b6b7
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-25 08:09:37 +0600

    feat: add basic context

--- projects/src/draft/13-submenu/Hero.js
+++ projects/src/draft/13-submenu/Hero.js
@@ -1,6 +1,10 @@
 import phoneImg from "./images/phone.svg";
+import { useGlobalContext } from "./context";
 
 const Hero = () => {
+  const data = useGlobalContext();
+  console.log("data>>", data);
+
   return <div>Hero</div>;
 };
 
--- projects/src/draft/13-submenu/context.js
+++ projects/src/draft/13-submenu/context.js
@@ -1,2 +1,44 @@
 import React, { useState, useContext } from "react";
 import sublinks from "./data";
+
+export const AppContext = React.createContext();
+
+export const AppProvider = ({ children }) => {
+  const [isSidebarOpen, setIsSidebarOpen] = useState(true);
+  const [isSubmenuOpen, setIsSubmenuOpen] = useState(true);
+
+  const openSidebar = () => {
+    setIsSidebarOpen(true);
+  };
+
+  const closeSidebar = () => {
+    setIsSidebarOpen(false);
+  };
+
+  const openSubmenu = () => {
+    setIsSubmenuOpen(true);
+  };
+
+  const closeSubmenu = () => {
+    setIsSubmenuOpen(false);
+  };
+
+  return (
+    <AppContext.Provider
+      value={{
+        isSidebarOpen,
+        isSubmenuOpen,
+        openSidebar,
+        openSubmenu,
+        closeSidebar,
+        closeSubmenu,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
