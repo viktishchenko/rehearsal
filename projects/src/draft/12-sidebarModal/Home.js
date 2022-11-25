4a42ca1376b5361a437b92c287162c665849e3ba
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-25 07:02:01 +0600

    feat: useContext hook in action

--- projects/src/draft/12-sidebarModal/Home.js
+++ projects/src/draft/12-sidebarModal/Home.js
@@ -1,18 +1,17 @@
-import { useContext } from "react";
 import { FaBars } from "react-icons/fa";
-import { AppContext, useGlobalContext } from "./context";
+import { useGlobalContext } from "./context";
 
 const Home = () => {
-  const data = useGlobalContext();
-  // const data = useContext(AppContext);
-  console.log(data); // halo
+  const { openSidebar, openModal } = useGlobalContext();
 
   return (
     <main>
-      <button className="sidebar-toggle">
+      <button className="sidebar-toggle" onClick={openSidebar}>
         <FaBars />
       </button>
-      <button className="btn">show modal</button>
+      <button className="btn" onClick={openModal}>
+        show modal
+      </button>
     </main>
   );
 };
--- projects/src/draft/12-sidebarModal/Sidebar.js
+++ projects/src/draft/12-sidebarModal/Sidebar.js
@@ -1,13 +1,16 @@
 import logo from "../11-navbar/logo.svg";
 import { FaTimes } from "react-icons/fa";
 import { social, links } from "./data";
+import { useGlobalContext } from "./context";
 
 const Sidebar = () => {
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
   return (
-    <aside className={`sidebar show-sidebar `}>
+    <aside className={`${isSidebarOpen ? "sidebar show-sidebar" : "sidebar"} `}>
       <div className="sidebar-header">
         <img src={logo} alt="logo" className="logo" />
-        <button className="close-btn">
+        <button className="close-btn" onClick={closeSidebar}>
           <FaTimes />
         </button>
       </div>
