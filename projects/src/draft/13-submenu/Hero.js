76e63a0a8f522c9d3c7dbfc0e1e8a6409e03b3d8
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-25 11:56:19 +0600

    feat: add navbar, hero layout

--- projects/src/draft/13-submenu/Hero.js
+++ projects/src/draft/13-submenu/Hero.js
@@ -2,10 +2,26 @@ import phoneImg from "./images/phone.svg";
 import { useGlobalContext } from "./context";
 
 const Hero = () => {
-  const data = useGlobalContext();
-  console.log("data>>", data);
+  const { closeSubmenu } = useGlobalContext();
 
-  return <div>Hero</div>;
+  return (
+    <section className="hero">
+      <div className="hero-center">
+        <article className="hero-info">
+          <h1>Payments infrastructure for the internet</h1>
+          <p>
+            Million of companies of all size-from startups to Fortune 500s-use
+            Stripe's software and APIs to accept payments, send payouts, and
+            manage their businesses online.
+          </p>
+          <button className="btn">Start now</button>
+        </article>
+        <article className="hero-image">
+          <img src={phoneImg} alt="phone" className="phone-img" />
+        </article>
+      </div>
+    </section>
+  );
 };
 
 export default Hero;
--- projects/src/draft/13-submenu/Navbar.js
+++ projects/src/draft/13-submenu/Navbar.js
@@ -1,8 +1,33 @@
 import logo from "./images/logo.svg";
 import { FaBars } from "react-icons/fa";
+import { useGlobalContext } from "./context";
 
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { openSidebar, openSubmenu, closeSubmenu } = useGlobalContext();
+  return (
+    <nav className="nav">
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="logo" className="nav-logo" />
+          <button className="btn toggle-btn" onClick={openSidebar}>
+            <FaBars />
+          </button>
+        </div>
+        <ul className="nav-links">
+          <li>
+            <button className="link-btn">product</button>
+          </li>
+          <li>
+            <button className="link-btn">developers</button>
+          </li>
+          <li>
+            <button className="link-btn">company</button>
+          </li>
+        </ul>
+        <button className="btn signin-btn">sign in</button>
+      </div>
+    </nav>
+  );
 };
 
 export default Navbar;
