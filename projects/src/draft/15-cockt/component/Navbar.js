81bc23be8471a7af2389226eda478c27257ad0bd
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-27 17:39:00 +0600

    feat: add basic routes

--- projects/src/draft/15-cockt/component/Navbar.js
+++
@@ -1,8 +0,0 @@
-import { Link } from "react-router-dom";
-import logo from "../logo.svg";
-
-const Navbar = () => {
-  return <div>Navbar</div>;
-};
-
-export default Navbar;
--- projects/src/index.js
+++ projects/src/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./draft/14-cart/css/index.css";
+import "./draft/15-cockt/css/index.css";
 import { AppProvider } from "./draft/15-cockt/context";
 import App from "./App";
 
