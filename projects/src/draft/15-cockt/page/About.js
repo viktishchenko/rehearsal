314199930971c132810ba3f21ea81c14683b3261
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-27 18:11:59 +0600

    feat: add basics styles

--- projects/src/draft/15-cockt/page/About.js
+++ projects/src/draft/15-cockt/page/About.js
@@ -1,5 +1,15 @@
 const About = () => {
-  return <div>About</div>;
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur
+        quidem ipsum sit commodi! Quo, labore doloribus! Quod tempora porro
+        blanditiis? Quibusdam asperiores cum vero praesentium dolor
+        perspiciatis, maxime veniam quia!
+      </p>
+    </section>
+  );
 };
 
 export default About;
--- projects/src/draft/15-cockt/page/NotFound.js
+++ projects/src/draft/15-cockt/page/NotFound.js
@@ -1,7 +1,16 @@
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <section className="error-page section">
+      <div className="error-container">
+        <h1>NotFound</h1>
+        <Link to="/" className="btn btn-primary">
+          back home
+        </Link>
+      </div>
+    </section>
+  );
 };
 
 export default NotFound;
