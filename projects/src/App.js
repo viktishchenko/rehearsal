5fb5f6a39d8acf856e6bd646f32876ac9845024f
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-28 19:01:54 +0600

    feat: basic states

--- projects/src/App.js
+++ projects/src/App.js
@@ -13,6 +13,17 @@ const url = "https://randomuser.me/api/";
 const defaultImage = "https://randomuser.me/api/portraits/men/75.jpg";
 
 function App() {
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const [title, setTitle] = useState("name");
+  const [value, setValue] = useState("random person");
+
+  /* get data fetch */
+  const handleValue = (e) => {
+    e.preventDefault();
+    console.log(e.target);
+  };
+
   return <h2>random user starter</h2>;
 }
 
