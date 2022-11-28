6b351dd788ff633959625447e5d3fab6ac10f4f3
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-28 09:10:03 +0600

    feat: useRef pass val to API, focus input

--- projects/src/draft/15-cockt/components/SearchForm.js
+++ projects/src/draft/15-cockt/components/SearchForm.js
@@ -1,9 +1,37 @@
+import React from "react";
 import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
   const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
 
-  return <div>SearchForm</div>;
+  React.useEffect(() => {
+    searchValue.current.focus();
+  }, []);
+
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <section className="section search">
+      <form className="search-form" onSubmit={handleSubmit}>
+        <div className="form-control">
+          <label htmlFor="name">search your favorite cocktail</label>
+          <input
+            type="text"
+            id="name"
+            ref={searchValue}
+            onChange={searchCocktail}
+          />
+        </div>
+      </form>
+    </section>
+  );
 };
 
 export default SearchForm;
