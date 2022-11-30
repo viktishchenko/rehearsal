ab8d36f2c62d5d4d2d0288b4200332f15173867b
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-30 11:58:23 +0600

    feat: add env

--- projects/src/App.js
+++ projects/src/App.js
@@ -2,12 +2,32 @@ import { useState, useEffect } from "react";
 import Photo from "./draft/19-stock/Photo.js";
 import { FaSearch } from "react-icons/fa";
 
-// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
-
 const mainUrl = "https://api.unsplash.com/photos/";
 const searchUrl = "https://api.unsplash.com/search/photos";
+const clientID = `?client_id=${process.env.REACT_APP_19_ACCESS_KEY}`;
 
 function App() {
+  const [loading, setLoading] = useState(false);
+  const [photos, setPhotos] = useState([]);
+
+  const fetchImages = async () => {
+    setLoading(true);
+    let url;
+    url = `${mainUrl}${clientID}`;
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      console.log("data>>", data);
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchImages();
+  }, []);
+
   return (
     <>
       <h2>stock project</h2>
