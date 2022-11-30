65ca2a6d45ae9c520e05b2fd8100a05fdf3bcce5
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-11-30 08:41:33 +0600

    ini: stock-photo pr

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,75 +1,18 @@
 import { useState, useEffect } from "react";
-import { useFetch } from "./draft/18-pagination/useFetch";
-import Follower from "./draft/18-pagination/Follower";
+import Photo from "./draft/19-stock/Photo.js";
+import { FaSearch } from "react-icons/fa";
+
+// const clientID = `?client_id=${process.env.REACT_APP_ACCESS_KEY}`
+
+const mainUrl = "https://api.unsplash.com/photos/";
+const searchUrl = "https://api.unsplash.com/search/photos";
 
 function App() {
-  const { loading, data } = useFetch();
-  const [page, setPage] = useState(0);
-  const [followers, setFollowers] = useState([]);
-
-  useEffect(() => {
-    if (loading) return;
-    setFollowers(data[page]);
-  }, [loading, page]);
-
-  const nextPage = () => {
-    setPage((oldPage) => {
-      let nextPage = oldPage + 1;
-      if (nextPage > data.length - 1) {
-        nextPage = 0;
-      }
-      return nextPage;
-    });
-  };
-  const prevPage = () => {
-    setPage((oldPage) => {
-      let prevPage = oldPage - 1;
-      if (prevPage < 0) {
-        prevPage = data.length - 1;
-      }
-      return prevPage;
-    });
-  };
-
-  const handlePage = (index) => {
-    setPage(index);
-  };
-
   return (
-    <main>
-      <div className="section-title">
-        <h1>{loading ? "loading..." : "pagination"}</h1>
-        <div className="underline"></div>
-      </div>
-      <section className="followers">
-        <div className="container">
-          {followers.map((follower) => {
-            return <Follower key={follower.id} {...follower} />;
-          })}
-        </div>
-        {!loading && (
-          <div className="btn-container">
-            <button className="prev-btn" onClick={prevPage}>
-              prev
-            </button>
-            {data.map((item, idx) => {
-              return (
-                <button
-                  key={idx}
-                  className={`page-btn ${idx === page ? "active-btn" : null}`}
-                  onClick={() => handlePage(idx)}
-                >
-                  {idx + 1}
-                </button>
-              );
-            })}
-            <button className="next-btn" onClick={nextPage}>
-              next
-            </button>
-          </div>
-        )}
-      </section>
-    </main>
+    <>
+      <h2>stock project</h2>
+      <Photo />
+    </>
   );
 }
 
