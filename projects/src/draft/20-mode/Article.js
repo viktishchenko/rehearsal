25054ff0d0ffaa13909ce1f386c4f4e4805286f5
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-01 07:51:43 +0600

    init: dark mode pr

---
+++ projects/src/draft/20-mode/Article.js
@@ -0,0 +1,7 @@
+import moment from "moment";
+
+const Article = () => {
+  return <div>Article</div>;
+};
+
+export default Article;
--- projects/src/index.js
+++ projects/src/index.js
@@ -1,11 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./draft/19-stock/css/index.css";
+import "./draft/20-mode/css/index.css";
 import App from "./App";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  // <React.StrictMode>
-  <App />
-  // </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
