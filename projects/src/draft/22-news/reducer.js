d4043e7314c42c91c63c0194de620cf58dcbc9c0
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-02 07:57:45 +0600

    refactor: if statement to switch, err action type

--- projects/src/draft/22-news/reducer.js
+++ projects/src/draft/22-news/reducer.js
@@ -7,11 +7,17 @@ import {
 } from "./actions";
 
 const reducer = (state, action) => {
-  if (action.type === SET_LOADING) {
+  /*   if (action.type === SET_LOADING) {
     return { ...state, isLoading: true };
   }
+  throw new Error(`no matching "${action.type}" action type`); */
 
-  throw new Error(`no matching "${action.type}" action type`);
+  switch (action.type) {
+    case "SET_LOADING":
+      return { ...state, isLoading: true };
+    default:
+      throw new Error(`no matching "${action.type}" action type`);
+  }
 };
 
 export default reducer;
