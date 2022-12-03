181cd798c70496b47b2e1c6bba34735d81db6ebb
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-03 17:59:40 +0600

    feat: add context val, init setup

--- projects/src/App.js
+++ projects/src/App.js
@@ -1,14 +1,21 @@
 import SetupForm from "./draft/23-quiz/SetupForm";
 import Loading from "./draft/23-quiz/Loading";
 import Modal from "./draft/23-quiz/Modal";
+import { useGlobalContext } from "./draft/23-quiz/context";
 
 function App() {
+  const { waiting, loading, questions, index, correct } = useGlobalContext();
+
+  if (waiting) {
+    return <SetupForm />;
+  }
+
+  if (loading) {
+    return <Loading />;
+  }
   return (
     <>
       <h2>quiz project</h2>
-      <SetupForm />
-      {/* <Loading /> */}
-      <Modal />
     </>
   );
 }
--- projects/src/draft/23-quiz/context.js
+++ projects/src/draft/23-quiz/context.js
@@ -11,10 +11,36 @@ const API_ENDPOINT = "https://opentdb.com/api.php?";
 
 const url = "";
 
+const tempUrl =
+  "https://opentdb.com/api.php?amount=10&category=21&difficulty=easy&type=multiple";
+
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value="halo">{children}</AppContext.Provider>;
+  const [waiting, setWaiting] = useState(true);
+  const [loading, setLoading] = useState(false);
+  const [questions, setQuestions] = useState([]);
+  const [index, setIndex] = useState(0);
+  const [correct, setCorrect] = useState(0);
+  const [error, setError] = useState(false);
+
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  return (
+    <AppContext.Provider
+      value={{
+        waiting,
+        loading,
+        questions,
+        index,
+        correct,
+        error,
+        isModalOpen,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
 };
 
 // custom useContext hook
