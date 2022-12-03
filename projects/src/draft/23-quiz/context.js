5d4e3d01bb0e840771c6371452dd487c980f9fc8
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-03 18:20:54 +0600

    feat: add fetch,display data logic

--- projects/src/draft/23-quiz/context.js
+++ projects/src/draft/23-quiz/context.js
@@ -26,6 +26,33 @@ const AppProvider = ({ children }) => {
 
   const [isModalOpen, setIsModalOpen] = useState(false);
 
+  const fetchQuestions = async (url) => {
+    setLoading(true);
+    setWaiting(false);
+    const response = await axios(url).catch((err) => {
+      console.log(err);
+    });
+    if (response) {
+      const data = response.data.results;
+      if (data.length > 0) {
+        setQuestions(data);
+        setLoading(false);
+        setWaiting(false);
+        setError(false);
+      } else {
+        setWaiting(true);
+        setError(true);
+      }
+    } else {
+      setWaiting(true);
+    }
+  };
+  console.log("questions>>", questions);
+
+  useEffect(() => {
+    fetchQuestions(tempUrl);
+  }, []);
+
   return (
     <AppContext.Provider
       value={{
