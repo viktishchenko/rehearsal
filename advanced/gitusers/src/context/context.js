65d204c17b114d431761c7eeff49281f6ae96954
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-05 12:40:12 +0600

    feat: create arr obj/itr through, render cmpt w mock,pass props, glob context

--- advanced/gitusers/src/context/context.js
+++ advanced/gitusers/src/context/context.js
@@ -9,12 +9,12 @@ const rootUrl = "https://api.github.com";
 const GithubContext = React.createContext();
 
 const GithubProvider = ({ children }) => {
-  const [githuUser, setGithuUser] = useState(mockUser);
+  const [githubUser, setGithuUser] = useState(mockUser);
   const [repos, setRepos] = useState(mockRepos);
   const [followers, setFollowers] = useState(mockFollowers);
 
   return (
-    <GithubContext.Provider value={{ githuUser, repos, followers }}>
+    <GithubContext.Provider value={{ githubUser, repos, followers }}>
       {children}
     </GithubContext.Provider>
   );
