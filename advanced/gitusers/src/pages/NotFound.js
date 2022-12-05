1b9a2e375c5bd45a45f9d5ec3d7cbe67cd69f270
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-05 10:46:36 +0600

    feat: add styled-404-component

--- advanced/gitusers/src/pages/NotFound.js
+++ advanced/gitusers/src/pages/NotFound.js
@@ -2,7 +2,17 @@ import styled from "styled-components";
 import { Link } from "react-router-dom";
 
 const NotFound = () => {
-  return <div>NotFound</div>;
+  return (
+    <Wrapper>
+      <div>
+        <h1>404</h1>
+        <h3>sorry,the page you tried cannot be found</h3>
+        <Link to="/" className="btn">
+          back home
+        </Link>
+      </div>
+    </Wrapper>
+  );
 };
 
 const Wrapper = styled.section`
