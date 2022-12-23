694d60b0eea74e7388b15d3b951953cce1063595
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-23 09:04:41 +0600

    feat: useReducer w load, get, err handler

--- large/comfy/src/context/products_context.js
+++ large/comfy/src/context/products_context.js
@@ -15,6 +15,10 @@ import {
 
 const initialState = {
   isSidebarOpen: false,
+  products_loading: false,
+  products_error: false,
+  products: [],
+  feature_products: [],
 };
 
 const ProductsContext = React.createContext();
@@ -31,8 +35,15 @@ export const ProductsProvider = ({ children }) => {
   };
 
   const fetchProducts = async (url) => {
-    const response = await axios.get(url);
-    console.log("response>>", response);
+    dispatch({ type: GET_PRODUCTS_BEGIN });
+    try {
+      const response = await axios.get(url);
+      const products = response.data;
+      console.log("products>>", products);
+      dispatch({ type: GET_PRODUCTS_SUCCESS, payload: products });
+    } catch (error) {
+      dispatch({ type: GET_PRODUCTS_ERROR });
+    }
   };
 
   useEffect(() => {
