7dd5575268b283c097d3e03b48a3a09f8769a56c
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-23 07:11:43 +0600

    feat: add subscribe component

--- large/comfy/src/components/Contact.js
+++ large/comfy/src/components/Contact.js
@@ -1,7 +1,31 @@
 import styled from "styled-components";
 
 const Contact = () => {
-  return <h4>contact section</h4>;
+  return (
+    <Wrapper>
+      <div className="section-center">
+        <h3>Join our newsletter and get 20% off</h3>
+        <div className="content">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Facilis
+            facere vel animi non nisi ut totam perspiciatis aperiam voluptatum
+            blanditiis.
+          </p>
+          {/* [SQ:form servises], Ex: action="https://formspree.io" */}
+          <form className="contact-form">
+            <input
+              type="email"
+              className="form-input"
+              placeholder="enter email"
+            />
+            <button type="submit" className="submit-btn">
+              subscribe
+            </button>
+          </form>
+        </div>
+      </div>
+    </Wrapper>
+  );
 };
 const Wrapper = styled.section`
   padding: 5rem 0;
