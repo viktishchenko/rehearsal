ebf92e5eb5bc331efafa241f4484169329d652f9
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-24 18:37:18 +0600

    feat: amount logic w lock value

--- large/comfy/src/components/AddToCart.js
+++ large/comfy/src/components/AddToCart.js
@@ -14,8 +14,24 @@ const AddToCart = ({ product }) => {
    */
   const [amount, setAmount] = useState(1);
 
-  const increase = () => {};
-  const decrease = () => {};
+  const increase = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount + 1;
+      if (tempAmount > stock) {
+        tempAmount = stock;
+      }
+      return tempAmount;
+    });
+  };
+  const decrease = () => {
+    setAmount((oldAmount) => {
+      let tempAmount = oldAmount - 1;
+      if (tempAmount < 1) {
+        tempAmount = 1;
+      }
+      return tempAmount;
+    });
+  };
 
   return (
     <Wrapper>
