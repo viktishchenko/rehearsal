51636ce2f9cdd73bc5060ae15b20fc43c7b1a1cc
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-24 06:48:31 +0600

    feat: component props depn/render

--- large/comfy/src/pages/SingleProductPage.js
+++ large/comfy/src/pages/SingleProductPage.js
@@ -64,6 +64,34 @@ const SingleProductPage = () => {
   return (
     <Wrapper>
       <PageHero title={name} product />
+      <div className="section section-center page">
+        <Link to="/products" className="btn">
+          back to products
+        </Link>
+        <div className="product-center">
+          <ProductImages />
+          <section className="content">
+            <h2>{name}</h2>
+            <Stars />
+            <h5 className="price">{formatPrice(price)}</h5>
+            <p className="desc">{description}</p>
+            <p className="info">
+              <span>Available : </span>
+              {stock > 0 ? "In stock" : "out of stock"}
+            </p>
+            <p className="info">
+              <span>SCU : </span>
+              {sku}
+            </p>
+            <p className="info">
+              <span>Brand : </span>
+              {company}
+            </p>
+            <hr />
+            {stock > 0 && <AddToCart />}
+          </section>
+        </div>
+      </div>
     </Wrapper>
   );
 };
