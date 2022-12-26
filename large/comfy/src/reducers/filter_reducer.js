6eafbbf4c68edfc843932e27daa4f3ca202bea1a
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-26 14:25:36 +0600

    feat: sort by price/a-z

--- large/comfy/src/reducers/filter_reducer.js
+++ large/comfy/src/reducers/filter_reducer.js
@@ -28,19 +28,33 @@ const filter_reducer = (state, action) => {
   }
   if (action.type === SORT_PRODUCTS) {
     const { sort, filtered_products } = state;
+    let tempProducts = [...filtered_products];
     if (sort === "price-lowest") {
-      console.log("price-lowest");
+      tempProducts = tempProducts.sort((a, b) => {
+        // long version
+        if (a.price < b.price) {
+          return -1;
+        }
+        if (a.price > b.price) {
+          return 1;
+        }
+        return 0;
+      });
     }
     if (sort === "price-highest") {
-      console.log("price-highest");
+      tempProducts = tempProducts.sort((a, b) => b.price - a.price);
     }
     if (sort === "name-a") {
-      console.log("name-a");
+      tempProducts = tempProducts.sort((a, b) => {
+        return a.name.localeCompare(b.name);
+      });
     }
     if (sort === "name-z") {
-      console.log("name-z");
+      tempProducts = tempProducts.sort((a, b) => {
+        return b.name.localeCompare(a.name);
+      });
     }
-    return { ...state };
+    return { ...state, filtered_products: tempProducts };
   }
 
   throw new Error(`No Matching "${action.type}" - action type`);
