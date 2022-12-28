2d0a28ab7fd91ac3099e2c202a1492d8dba75c42
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-29 05:32:52 +0600

    feat: cart amount w cntxt, reducer & reduce()

--- large/comfy/src/components/CartButtons.js
+++ large/comfy/src/components/CartButtons.js
@@ -7,6 +7,7 @@ import { useUserContext } from "../context/user_context";
 
 const CartButtons = () => {
   const { closeSidebar } = useProductsContext();
+  const { total_items } = useCartContext();
 
   return (
     <Wrapper className="cart-btn-wrapper">
@@ -14,7 +15,7 @@ const CartButtons = () => {
         Cart
         <span className="cart-container">
           <FaShoppingCart />
-          <span className="cart-value">12</span>
+          <span className="cart-value">{total_items}</span>
         </span>
       </Link>
       <button className="auth-btn" onClick={closeSidebar}>
