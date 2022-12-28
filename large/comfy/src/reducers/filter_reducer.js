b5f6a234fe71b1bbd8ceef712f102e22d17fd5b2
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-28 11:46:56 +0600

    feat: add text filtering

--- large/comfy/src/reducers/filter_reducer.js
+++ large/comfy/src/reducers/filter_reducer.js
@@ -70,7 +70,18 @@ const filter_reducer = (state, action) => {
     };
   }
   if (action.type === FILTER_PRODUCTS) {
-    return { ...state };
+    const { all_products } = state;
+    const { text, category, company, color, price, shipping } = state.filters;
+
+    let tempProducts = [...all_products];
+    // filtering ...
+    // search (text)
+    if (text) {
+      tempProducts = tempProducts.filter((product) => {
+        return product.name.toLowerCase().startsWith(text);
+      });
+    }
+    return { ...state, filtered_products: tempProducts };
   }
   if (action.type === CLEAR_FILTERS) {
     console.log("halo2");
