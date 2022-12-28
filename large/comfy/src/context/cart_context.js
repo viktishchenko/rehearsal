43ec926bd9357e1d8fc167322fcae1f6becd2e3c
Viktor Tishchenko <9043258451vvt@gmail.com>
2022-12-28 18:57:51 +0600

    feat: clear/remove w cntxt & reducer

--- large/comfy/src/context/cart_context.js
+++ large/comfy/src/context/cart_context.js
@@ -35,13 +35,17 @@ export const CartProvider = ({ children }) => {
   };
 
   // remove item
-  const removeItem = (id) => {};
+  const removeItem = (id) => {
+    dispatch({ type: REMOVE_CART_ITEM, payload: id });
+  };
 
   // toggleAmount
   const toggleAmount = (id, value) => {};
 
   // clearCart
-  const clearCart = () => {};
+  const clearCart = () => {
+    dispatch({ type: CLEAR_CART });
+  };
 
   useEffect(() => {
     localStorage.setItem("cart", JSON.stringify(state.cart));
