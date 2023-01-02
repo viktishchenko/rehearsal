5482c720da904080cc46275d319b5332957df79e
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-02 19:29:58 +0600

    feat: add first redux reducer

--- large/redux/src/components/CartContainer.js
+++ large/redux/src/components/CartContainer.js
@@ -1,7 +1,9 @@
 import CartItem from "./CartItem";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
 
 const CartContainer = () => {
+  const dispatch = useDispatch();
   const { amount, cartItems } = useSelector((store) => {
     return store.cart;
   });
@@ -38,7 +40,14 @@ const CartContainer = () => {
             total <span>$0.00</span>
           </h4>
         </div>
-        <button className="btn clear-btn">clear cart</button>
+        <button
+          className="btn clear-btn"
+          onClick={() => {
+            dispatch(clearCart());
+          }}
+        >
+          clear cart
+        </button>
       </footer>
     </section>
   );
--- large/redux/src/features/cart/cartSlice.js
+++ large/redux/src/features/cart/cartSlice.js
@@ -11,6 +11,13 @@ const initialState = {
 const cartSlice = createSlice({
   name: "cart",
   initialState,
+  reducers: {
+    clearCart: (state) => {
+      state.cartItems = [];
+    },
+  },
 });
+
+export const { clearCart } = cartSlice.actions;
 
 export default cartSlice.reducer;
