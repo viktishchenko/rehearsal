8ee78a3a3a628a144ee9322a1674b7a853f3027c
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-03 08:46:11 +0600

    feat: add modal logic

--- large/redux/src/components/CartContainer.js
+++ large/redux/src/components/CartContainer.js
@@ -1,6 +1,6 @@
 import CartItem from "./CartItem";
 import { useDispatch, useSelector } from "react-redux";
-import { clearCart } from "../features/cart/cartSlice";
+import { openModal } from "../features/modal/modalSlice";
 
 const CartContainer = () => {
   const dispatch = useDispatch();
@@ -43,7 +43,7 @@ const CartContainer = () => {
         <button
           className="btn clear-btn"
           onClick={() => {
-            dispatch(clearCart());
+            dispatch(openModal());
           }}
         >
           clear cart
--- large/redux/src/components/Modal.js
+++ large/redux/src/components/Modal.js
@@ -1,11 +1,32 @@
+import { useDispatch } from "react-redux";
+import { clearCart } from "../features/cart/cartSlice";
+import { closeModal } from "../features/modal/modalSlice";
+
 const Modal = () => {
+  const dispatch = useDispatch();
+
   return (
     <aside className="modal-container">
       <div className="modal">
         <h4>remove all items from your shopping cart?</h4>
         <div className="btn-container">
-          <button className="btn confirm-btn">confirm</button>
-          <button className="btn clear-btn">cancel</button>
+          <button
+            className="btn confirm-btn"
+            onClick={() => {
+              dispatch(clearCart());
+              dispatch(closeModal());
+            }}
+          >
+            confirm
+          </button>
+          <button
+            className="btn clear-btn"
+            onClick={() => {
+              dispatch(closeModal());
+            }}
+          >
+            cancel
+          </button>
         </div>
       </div>
     </aside>
