10c85fa122f64088a129a9594f5007bc0656bd64
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-05 11:03:26 +0600

    feat: add styled component

--- reduxone/src/pages/Landing.js
+++ reduxone/src/pages/Landing.js
@@ -1,9 +1,10 @@
 import logo from "../assets/images/logo.svg";
 import main from "../assets/images/main.svg";
+import styled from "styled-components";
 
 const Landing = () => {
   return (
-    <main>
+    <Wrapper>
       <nav>
         <img src={logo} alt="logo" className="logo" />
       </nav>
@@ -22,8 +23,10 @@ const Landing = () => {
         </div>
         <img src={main} alt="job app" className="img main-img" />
       </div>
-    </main>
+    </Wrapper>
   );
 };
+
+const Wrapper = styled.main``;
 
 export default Landing;
