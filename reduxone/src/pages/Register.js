25a679e47d15bb7bf7c8b46fd451ebee3f924c3a
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-05 17:07:07 +0600

    feat: add controlled input

--- reduxone/src/pages/Register.js
+++ reduxone/src/pages/Register.js
@@ -1,5 +1,50 @@
+import { useState, useEffect } from "react";
+import Wrapper from "../assets/wrappers/RegisterPage";
+import { Logo } from "../components";
+
+const initialState = {
+  name: "",
+  email: "",
+  password: "",
+  isMember: true,
+};
+
 const Register = () => {
-  return <div>Register</div>;
+  const [values, setValues] = useState(initialState);
+
+  const handlChange = (e) => {
+    console.log("e.target>>", e.target);
+  };
+
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("e.target>>", e.target);
+  };
+
+  return (
+    <Wrapper className="full-page">
+      <form className="form" onSubmit={onSubmit}>
+        <Logo />
+        <h3>Login</h3>
+        {/* name field */}
+        <div className="form-row">
+          <label htmlFor="name" className="form-label">
+            name
+          </label>
+          <input
+            type="text"
+            name="name"
+            value={values.name}
+            onChange={handlChange}
+            className="form-input"
+          />
+        </div>
+        <button type="submit" className="btn btn-block">
+          submit
+        </button>
+      </form>
+    </Wrapper>
+  );
 };
 
 export default Register;
