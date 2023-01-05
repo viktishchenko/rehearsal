31d89cf0a322d9c8b832aee2e2b0f5ed711bbe07
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-05 19:22:41 +0600

    feat: check inputs values

--- reduxone/src/pages/Register.js
+++ reduxone/src/pages/Register.js
@@ -13,12 +13,17 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
 
   const handleChange = (e) => {
-    console.log("e.target>>", e.target);
+    const name = e.target.name;
+    const value = e.target.value;
+    setValues({ ...values, [name]: value });
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log("e.target>>", e.target);
+    const { name, email, password, isMember } = values;
+    if (!email || !password || (!isMember && !name)) {
+      console.log("Please, fill out all fields");
+    }
   };
 
   const toggleMember = () => {
