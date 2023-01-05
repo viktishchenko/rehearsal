8c601496a6cef531f302deee1c7799ca383f105c
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-05 17:31:27 +0600

    feat: add form component

--- reduxone/src/pages/Register.js
+++ reduxone/src/pages/Register.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import Wrapper from "../assets/wrappers/RegisterPage";
-import { Logo } from "../components";
+import { Logo, FormRow } from "../components";
 
 const initialState = {
   name: "",
@@ -12,7 +12,7 @@ const initialState = {
 const Register = () => {
   const [values, setValues] = useState(initialState);
 
-  const handlChange = (e) => {
+  const handleChange = (e) => {
     console.log("e.target>>", e.target);
   };
 
@@ -27,18 +27,26 @@ const Register = () => {
         <Logo />
         <h3>Login</h3>
         {/* name field */}
-        <div className="form-row">
-          <label htmlFor="name" className="form-label">
-            name
-          </label>
-          <input
-            type="text"
-            name="name"
-            value={values.name}
-            onChange={handlChange}
-            className="form-input"
-          />
-        </div>
+        <FormRow
+          type="text"
+          name="name"
+          value={values.name}
+          handleChange={handleChange}
+        />
+        {/* email field */}
+        <FormRow
+          type="email"
+          name="email"
+          value={values.email}
+          handleChange={handleChange}
+        />
+        {/* password field */}
+        <FormRow
+          type="password"
+          name="password"
+          value={values.password}
+          handleChange={handleChange}
+        />
         <button type="submit" className="btn btn-block">
           submit
         </button>
