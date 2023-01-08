5f3626c66304164197dfa59c38a620ff2c2850e3
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-08 08:31:20 +0600

    feat: add redirect

--- reduxone/src/pages/Register.js
+++ reduxone/src/pages/Register.js
@@ -4,6 +4,7 @@ import { Logo, FormRow } from "../components";
 import { toast } from "react-toastify";
 import { useSelector, useDispatch } from "react-redux";
 import { loginUser, registerUser } from "../features/user/userSlice";
+import { useNavigate } from "react-router-dom";
 
 const initialState = {
   name: "",
@@ -16,6 +17,7 @@ const Register = () => {
   const [values, setValues] = useState(initialState);
   const { user, isLoading } = useSelector((store) => store.user);
   const dispatch = useDispatch();
+  const navigate = useNavigate();
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -45,6 +47,14 @@ const Register = () => {
   const toggleMember = () => {
     setValues({ ...values, isMember: !values.isMember });
   };
+
+  useEffect(() => {
+    if (user) {
+      setTimeout(() => {
+        navigate("/");
+      }, 3000);
+    }
+  }, [user]);
 
   return (
     <Wrapper className="full-page">
