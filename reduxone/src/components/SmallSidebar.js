fe523a359cab8c0f27e094ac76c1d63b87009e2a
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-09 13:55:56 +0600

    feat: add sidebar comp css

--- reduxone/src/components/SmallSidebar.js
+++ reduxone/src/components/SmallSidebar.js
@@ -1,9 +1,28 @@
 import Wrapper from "../assets/wrappers/SmallSidebar";
+import { FaTimes } from "react-icons/fa";
+import { NavLink } from "react-router-dom";
+import Logo from "./Logo";
+import { useSelector, useDispatch } from "react-redux";
 
 const SmallSidebar = () => {
   return (
     <Wrapper>
-      <h2>SmallSidebar</h2>
+      <div className="sidebar-container show-sidebar">
+        <div className="content">
+          <button
+            className="close-btn"
+            onClick={() => {
+              console.log("close menu");
+            }}
+          >
+            <FaTimes />
+          </button>
+          <header>
+            <Logo />
+          </header>
+          <div className="nav-links">nav links</div>
+        </div>
+      </div>
     </Wrapper>
   );
 };
