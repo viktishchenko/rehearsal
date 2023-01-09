117e5d66907b899974f6e8fe4cc95176b95ce583
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-09 18:07:58 +0600

    refactor: add nav component, fin sidebar

---
+++ reduxone/src/components/NavLinks.js
@@ -0,0 +1,28 @@
+import { NavLink } from "react-router-dom";
+import links from "../utils/links";
+
+const NavLinks = ({ toggle }) => {
+  return (
+    <div className="nav-links">
+      {links.map((link) => {
+        const { text, path, id, icon } = link;
+
+        return (
+          <NavLink
+            to={path}
+            className={({ isActive }) =>
+              isActive ? "nav-link active" : "nav-link"
+            }
+            key={id}
+            onClick={toggle}
+          >
+            <span className="icon">{icon}</span>
+            {text}
+          </NavLink>
+        );
+      })}
+    </div>
+  );
+};
+
+export default NavLinks;
--- reduxone/src/components/SmallSidebar.js
+++ reduxone/src/components/SmallSidebar.js
@@ -1,10 +1,9 @@
 import Wrapper from "../assets/wrappers/SmallSidebar";
 import { FaTimes } from "react-icons/fa";
-import { NavLink } from "react-router-dom";
 import Logo from "./Logo";
 import { useSelector, useDispatch } from "react-redux";
 import { toggleSidebar } from "../features/user/userSlice";
-import links from "../utils/links";
+import NavLinks from "./NavLinks";
 
 const SmallSidebar = () => {
   const { isSidebarOpen } = useSelector((store) => store.user);
@@ -28,25 +27,7 @@ const SmallSidebar = () => {
           <header>
             <Logo />
           </header>
-          <div className="nav-links">
-            {links.map((link) => {
-              const { text, path, id, icon } = link;
-
-              return (
-                <NavLink
-                  to={path}
-                  className={({ isActive }) =>
-                    isActive ? "nav-link active" : "nav-link"
-                  }
-                  key={id}
-                  onClick={toggle}
-                >
-                  <span className="icon">{icon}</span>
-                  {text}
-                </NavLink>
-              );
-            })}
-          </div>
+          <NavLinks toggle={toggle} />
         </div>
       </div>
     </Wrapper>
