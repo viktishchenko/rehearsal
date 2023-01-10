72c5c0e378ff0aa87e5fd433e16757117cd355f9
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-10 12:10:21 +0600

    feat: auth protect smth

--- reduxone/src/features/user/userSlice.js
+++ reduxone/src/features/user/userSlice.js
@@ -43,11 +43,16 @@ export const updateUser = createAsyncThunk(
     try {
       const res = await customFetch.patch("/auth/updateUser", user, {
         headers: {
-          authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
+          authorization: `Bearer `,
+          // authorization: `Bearer ${thunkAPI.getState().user.user.token}`,
         },
       });
       return res.data;
     } catch (error) {
+      if (error.response.status === 401) {
+        thunkAPI.dispatch(logoutUser());
+        return thunkAPI.rejectWithValue("Unauthorized! Logging out...");
+      }
       return thunkAPI.rejectWithValue(error.response.data.msg);
     }
   }
