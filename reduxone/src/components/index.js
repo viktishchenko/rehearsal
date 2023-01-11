4163a4be0900d7bc896072845909de3fefe38be8
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-11 19:53:22 +0600

    feat: handle user data components init

--- reduxone/src/components/index.js
+++ reduxone/src/components/index.js
@@ -4,5 +4,18 @@ import Navbar from "./Navbar";
 import BigSidebar from "./BigSidebar";
 import SmallSidebar from "./SmallSidebar";
 import FormRowSelect from "./FormRowSelect";
+import Job from "./Job";
+import JobsContainer from "./JobsContainer";
+import SearchContainer from "./SearchContainer";
 
-export { Logo, FormRow, Navbar, BigSidebar, SmallSidebar, FormRowSelect };
+export {
+  Logo,
+  JobsContainer,
+  SearchContainer,
+  Job,
+  FormRow,
+  Navbar,
+  BigSidebar,
+  SmallSidebar,
+  FormRowSelect,
+};
