7dd04ca940e7edc06fec73c97a4b9beb620b0b4f
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-01-11 06:33:35 +0600

    feat: add rdx clear inp func-ty

--- reduxone/src/pages/Dashboard/AddJob.js
+++ reduxone/src/pages/Dashboard/AddJob.js
@@ -2,7 +2,7 @@ import { FormRow, FormRowSelect } from "../../components";
 import Wrapper from "../../assets/wrappers/DashboardFormPage";
 import { useDispatch, useSelector } from "react-redux";
 import { toast } from "react-toastify";
-import { handleChange } from "../../features/job/jobSlice";
+import { handleChange, clearValues } from "../../features/job/jobSlice";
 
 const AddJob = () => {
   const {
@@ -83,7 +83,7 @@ const AddJob = () => {
               type="button"
               className="btn btn-block clear-btn"
               onClick={() => {
-                console.log("clear btn");
+                dispatch(clearValues());
               }}
             >
               clear
